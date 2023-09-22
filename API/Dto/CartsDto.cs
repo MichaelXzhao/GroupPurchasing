1b4e9d4885bcca412b76c012791924dd3267d970
MichaelXzhao <128780032+MichaelXzhao@users.noreply.github.com>
2023-09-22 11:32:22 +0800

    update 91api

---
+++ API/Dto/CartsDto.cs
@@ -0,0 +1,7 @@
+public class CartInsertRequestDto
+{
+    public int shopId { get; set; }
+    public int salePageId { get; set; }
+    public int saleProductSKUid { get; set; }
+    public int qty { get; set; }
+}

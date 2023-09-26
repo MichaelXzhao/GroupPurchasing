79d9abdb392f063d4e1be7a15d7a6dd21e6ed88f
winston <winston@gmail.com>
2023-09-26 17:18:13 +0800

    20230926

--- Page/src/pages/member/memberGroupBuy.vue
+++ Page/src/pages/member/memberGroupBuy.vue
@@ -4,6 +4,7 @@ import Loading from "../../components/Loading.vue"
 import axios from 'axios'
 import changeqty from '../../function/changeqty';
 import shareURL from '../../function/shareURL';
+import router from '../../router/router';
 
 export default{
     setup() {
@@ -31,7 +32,8 @@ export default{
                 "picture": data.value.picture,
                 "salepageid": data.value.salepageid,
                 "shopid": data.value.shopid,
-                "skuid": data.value.skuid
+                "skuid": data.value.skuid,
+                "recommender":localStorage.getItem("recommender")
             })
             .then(res=>{
                 console.log(res)
@@ -61,6 +63,9 @@ export default{
         }
         let shareWeb = ()=>{
             shareURL.shareURL(localStorage.getItem("username"))
+        }
+        let gotoProfile = ()=>{
+            router.push({path:"/profile"})
         }
         onMounted(() => {
             loadingDisplay.value = true
@@ -97,7 +102,8 @@ export default{
             addqty,
             reduceqty,
             joinGroupBuy,
-            shareWeb
+            shareWeb,
+            gotoProfile
         }
     },
     components: { Loading }
@@ -109,6 +115,11 @@ export default{
         <div class="layout-header-fix-logo">
             <img style="height: 100px;" src="../../img/169545314870657.png" alt="">
             <img class="header-logo" src="../../img/Group2.svg" alt="">
+        </div>
+        <div v-on:click="gotoProfile">
+            <svg style="width: 30px;margin-top: 40px;position: absolute;right: 150px; border-radius: 50%; border-style: solid; border-color: #fff;border-width: 2px;" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 256 256" enable-background="new 0 0 256 256" xml:space="preserve">
+            <g><g><g><path fill="#ffffff" d="M120.8,1c-7.2,0.8-17.6,3.8-24.1,7.1C77,18,63.6,35.5,58.6,57.6c-1.7,7.5-1.7,20.5,0,28.1c4.5,20.1,16.5,36.5,34.5,47c2.4,1.4,3.7,2.4,3.3,2.6c-0.4,0.2-3.4,1.2-6.7,2.4c-27.1,9.2-51.6,29.5-65.2,54c-7.8,14.1-12.6,29.6-14.1,45.6c-0.8,8.2-0.5,12.5,0.9,14.3c3.6,4.9,9.9,4.9,13.4,0c0.9-1.2,1.1-2.6,1.5-8.5c1-16.5,4.6-30,11.7-43.5c15.6-29.6,45-49.7,78.2-53.6c48.4-5.6,93.5,23,108.8,69.1c3.4,10.1,4.6,17.1,5.5,32.3c0.5,8.1,9.8,10.8,14.6,4.2c1-1.3,1.1-2.1,1-8.3c-0.3-27.1-12-54.7-32.1-75.5c-13.1-13.5-30.1-24.4-47.3-30.2c-3.3-1.1-6.3-2.2-6.7-2.3c-0.5-0.2,0.9-1.2,3.3-2.6c18.2-10.6,30.7-28,34.7-48.3c1.3-6.6,1.2-19.9-0.3-26.6c-6-28.1-26.4-48.8-54.4-55.3C138,1.2,126.1,0.5,120.8,1z M139.4,18c21.1,4.5,36.8,20,42.1,41.4c1.3,5.2,1.6,17.4,0.5,22.5c-2.5,11.3-7.1,20.2-14.7,28.1c-21.5,22.3-56.9,22.4-78.3,0.1c-7.7-8.1-12.4-16.9-14.8-28.2c-0.9-4.1-0.8-16.2,0-20.6c2.2-10.8,7.4-20.8,15.1-28.5c7.8-7.8,18.4-13.3,28.7-15c2.1-0.3,4.1-0.7,4.5-0.8C124.4,16.6,136.2,17.3,139.4,18z"/></g></g></g>
+            </svg>
         </div>
     </div>
     <div class="hostGroupBuy-content">
@@ -200,11 +211,11 @@ export default{
             </table>
             <div class="hostGroup-list-total">
                 <div class="hostGroup-list-total-total">
-                    <div class="hostGroup-list-total-content">
-                        <div style="margin-right: 10px;">已選購${{ discountData.totalOriginalPrice }}</div>
-                        <div style="margin-right: 10px;">目前折扣{{ discountData.promotionDiscount }}</div>
-                        <div style="margin-right: 10px;">{{ discountData.promotionDiscountTitle }}</div>
-                        <div style="margin-right: 10px;">{{ discountData.recommendConditionTitle }}</div>
+                    <div class="hostGroup-list-total-content" style="display: flex;">
+                        <div style="margin-right: 10px;font-size: 16px;align-items: end;display: flex;">已選購${{ discountData.totalOriginalPrice }}</div>
+                        <div style="margin-right: 10px;font-size: 16px;align-items: end;display: flex;">目前折扣{{ discountData.promotionDiscount }}</div>
+                        <div style="margin-right: 10px;font-size: 16px;align-items: end;display: flex;">{{ discountData.promotionDiscountTitle }}</div>
+                        <div style="margin-right: 10px;color: #70c1d6;align-items: end;display: flex;">{{ discountData.recommendConditionTitle }}</div>
                     </div>
                 </div>
             </div>
